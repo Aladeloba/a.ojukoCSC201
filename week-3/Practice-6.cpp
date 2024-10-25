b28ff6cb281cc519320aab7e549c090e7a1b09d0
a.ojukoCOS101 <aladelobaojuko@gmail.com>
2024-10-25 14:59:34 +0100

    My Second Commit

---
+++ week-3/Practice-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int x = 2;
+int main() {
+
+	if (x > 2)
+		cout << x << endl;
+	else
+		cout << "x is not greater than 2" << endl;
+	return 0;
+
+}
---
+++ week-3/Practice-7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int score = 60;
+
+int main() {
+	if(score>=70 && score<=100) {
+		cout << "Grade A" << endl;
+		cout << "Excellent" << endl;}
+	else if (score>=60 && score<70){
+		cout << "Grade B" << endl << "Very Good" << endl;}
+	else if (score>=50 && score<60)
+		cout << "Grade C" << endl << "Good" << endl;
+	else if (score>=45 && score<50)
+		cout << "Grade D" << endl << "Average" << endl;
+	else if (score>=40 && score<45)
+		cout << "Grade E" << endl << "Pass" << endl;
+	else if (score>=0 && score<40)
+		cout << "Grade F" << endl << "Fail" << endl;
+	else
+		cout << "Enter Valid Score" << endl;
+		
+	return 0;
+}
